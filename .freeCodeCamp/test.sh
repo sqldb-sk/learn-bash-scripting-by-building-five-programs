d1a860cd6810f61428157a71fa7ddfbcbab4d0e9
sqldb-sk <kordic.simeon@gmail.com>
2024-12-08 11:52:55 +0000

    Save progress: 1412.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -9,13 +9,22 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 
 GET_FORTUNE() {
-  echo Ask a yes or no question:
+  
+  if [[ ! $1 ]]
+  then
+    echo Ask a yes or no question:
+  else
+    echo Try again. Make sure it ends with a question mark:
+  fi
+  read QUESTION
 }
 
-GET_FORTUNE
 
-read QUESTION
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE
+done
 
 echo ${RESPONSES[$N]}
 
-echo $QUESTION
+GET_FORTUNE again
