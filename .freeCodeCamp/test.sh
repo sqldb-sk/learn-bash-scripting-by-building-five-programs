7c21b2637587e0aac26d54c34a423ddfb5c5a3a9
sqldb-sk <kordic.simeon@gmail.com>
2024-12-08 11:09:50 +0000

    Save progress: 1110.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-#Bingo Number Generator
-echo -e "\n"~~ Bingo Number Generator ~~"\n"
+#Program to tell a persons fortune
 
-NUMBER=$(( RANDOM % 15+61 ))
+echo -e "\n"~~ Fortune Teller ~~"\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
 
-
-TEXT="The next number is, "
-
-if (( NUMBER <= 15 ))
-then
-  echo $TEXT B:$NUMBER
-elif [[ $NUMBER -le 30 ]]
-then
-  echo $TEXT I:$NUMBER
-elif (( NUMBER < 46 ))
-then
-  echo $TEXT N:$NUMBER  
-elif [[ $NUMBER -lt 61 ]]
-then
-  echo $TEXT G:$NUMBER
-else 
-  echo $TEXT O:$NUMBER
-fi
+declare -p RESPONSES
