2ebb9f79a8ad6b7bcd4ec520eb7ae200286e346b
sqldb-sk <kordic.simeon@gmail.com>
2024-12-08 10:25:39 +0000

    Save progress: 817.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Program that counts down to zero from a given argument
+#Bingo Number Generator
+echo -e "\n"~~ Bingo Number Generator ~~"\n"
 
-echo -e "\n"~~ Countdown Timer ~~"\n"
+NUMBER=5
 
-if [[ $1 -gt 0 ]]
-then
-    : 'for (( i = $1; i >= 0; i-- ))
-    do
-         echo $i 
-         sleep 1
-    done
-    '
-    I=$1
-else
-  echo "Include a positive integer as the first argument."
-fi
 
-echo $I
+echo $NUMBER
